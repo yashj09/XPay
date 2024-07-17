8d5915a3ce44749a0ed4b028a0ecbb8ef2dbbd39
Yash Jain <yashj8858@gmail.com>
2024-07-17 16:51:52 +0530

    ui send money

--- frontend/src/Pages/Sendmoney.jsx
+++ frontend/src/Pages/Sendmoney.jsx
@@ -3,44 +3,67 @@ import { Label } from "../components/ui/label";
 import { Input } from "../components/ui/input";
 import { Button } from "../components/ui/button";
 import axios from "axios";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "@/components/ui/card";
 import { useSearchParams } from "react-router-dom";
-
 const Sendmoney = () => {
   const [searchParams] = useSearchParams();
   const [amount, setAmount] = useState(0);
   const id = searchParams.get("id");
   const name = searchParams.get("name");
+
   return (
-    <>
-      Sendmoney to {name}
-      <Input
-        placeholder="enter amount"
-        type="number"
-        onChange={(e) => {
-          setAmount(e.target.value);
-        }}
-      />
-      <Button
-        onClick={() => {
-          axios.post(
-            "http://localhost:3000/api/v1/account/transfer",
-            {
-              amount,
-              to: id,
-            },
-            {
-              headers: {
-                Authorization: localStorage.getItem("token"),
-              },
-            }
-          );
-          alert("send " + amount + "Rs to " + name);
-        }}
-      >
-        Send money
-      </Button>
-    </>
+    <div className="flex justify-center items-center h-[100dvh]">
+      <Card className="w-full max-w-sm  ">
+        <CardHeader>
+          <CardTitle>Send Money to {name}</CardTitle>
+          <CardDescription>Enter the amount to send.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form className="space-y-4">
+            <div>
+              <Label htmlFor="amount">Amount</Label>
+              <Input
+                id="amount"
+                type="number"
+                placeholder="Enter amount"
+                className="w-full"
+                onChange={(e) => {
+                  setAmount(e.target.value);
+                }}
+              />
+            </div>
+
+            <Button
+              type="submit"
+              className="w-full"
+              onClick={() => {
+                axios.post(
+                  "http://localhost:3000/api/v1/account/transfer",
+                  {
+                    amount,
+                    to: id,
+                  },
+                  {
+                    headers: {
+                      Authorization: localStorage.getItem("token"),
+                    },
+                  }
+                );
+                alert("send " + amount + "rs to " + name);
+              }}
+            >
+              Send
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
   );
 };
-
 export default Sendmoney;
